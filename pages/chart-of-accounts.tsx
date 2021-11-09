3b67c6685c244d52437ab8ab2e7a86cb09d89280
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-11-09 15:55:35 +0700

    feat(accounts): create fetchAccounts hook with react-query

--- pages/chart-of-accounts.tsx
+++ pages/chart-of-accounts.tsx
@@ -1,19 +1,41 @@
+import { useFetchAccounts } from '@api/accounts'
 import Layout from '@components/Layout'
 import { NavbarProps } from '@components/Navbar'
 import { Navigation, navigation } from '@constants/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 interface Props {}
 
-const navInfo: Navigation =
-  navigation.find((n) => n.name == 'Chart of Accounts') || ({} as Navigation)
+const navInfo: Navigation = navigation.find((n) => n.name == 'Chart of Accounts') || ({} as Navigation)
 const meta: NavbarProps = {
   title: navInfo.name,
   icon: navInfo.icon,
 }
 
 export const CoAPage = (props: Props) => {
-  return <Layout navbarProps={meta}>ini Chart of accounts</Layout>
+  const { isLoading, isError, data, error } = useFetchAccounts()
+
+  useEffect(() => {
+    console.log(data)
+  }, [data, isLoading])
+
+  if (isLoading) return <h1>Loading...</h1>
+
+  return (
+    <Layout navbarProps={meta}>
+      ini Chart of accounts
+      {data?.data.map((d) => (
+        <div key={`coa-${d.accountNumber}`} className="my-4">
+          <p>{d.accountNumber}</p>
+          <p>{d.name}</p>
+          <p>{d.description}</p>
+          <p>{d.category}</p>
+          <p>{d.normalBalance}</p>
+          <p>{d.type}</p>
+        </div>
+      ))}
+    </Layout>
+  )
 }
 
 export default CoAPage
---
+++ services/api/accounts/index.tsx
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import config from 'config';
+
+export interface Account {
+  id: number;
+  accountNumber: string;
+  name: string;
+  description: string;
+  category: string;
+  normalBalance: string;
+  type: string;
+}
+
+export const useFetchAccounts = () => {
+  return useQuery(
+    'accounts',
+    () => (
+      axios.get<Account[]>(`${config.API_URL_CARBON}/accounts`)
+    )
+  )
+}
