4fd6388f8e3f20890350c26d3174ce575f400b76
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-11-09 15:55:03 +0700

    feat(react-query): setup react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import '../styles/globals.css'
 
+
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient} contextSharing={true}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
