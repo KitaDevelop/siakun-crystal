35bcb548e79c0131cac1cf9c81cffa986a047684
asfiowilma <asfiolithaw@yahoo.co.id>
2021-11-18 19:40:49 +0700

    moved login redirect to /login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { GlobalProvider } from '@context/Global'
 import { AppProps } from 'next/app'
+import router from 'next/router'
+import { useEffect, useState } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [authorized, setAuthorized] = useState(true)
+
+  useEffect(() => {
+    if (!authorized) router.push('/login')
+  }, [authorized])
+
   return (
     <GlobalProvider>
       <Component {...pageProps} />
