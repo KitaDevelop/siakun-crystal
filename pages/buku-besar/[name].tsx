6353fbdd2c950119a7aebca906422d77d189be1f
asfiowilma <asfiolithaw@yahoo.co.id>
2022-06-14 13:53:41 +0700

    fix: adjust ssr and queries on auditor view

--- pages/buku-besar/[name].tsx
+++ pages/buku-besar/[name].tsx
@@ -1,15 +1,19 @@
+import { getUserProfile } from '@api/auth'
 import { getJournalEntriesByAccount } from '@api/entries/journal/endpoints'
 import BukuBesar from '@components/BukuBesar'
 import Layout from '@components/Layout'
 import { NavbarProps } from '@components/Navbar'
 import { navigation, Navigation } from '@constants/navigation'
+import { ROLE, UserProfile } from '@context/AuthContext/types'
 import { JournalEntry } from '@context/JournalEntryContext/types'
 import { useAccount } from '@hooks/useAccount'
+import useAuth from '@hooks/useAuth'
+import { useOrganization } from '@hooks/useOrganization'
 import { extractAccountInfo } from '@utils/extractAccountInfo'
 import axios from 'axios'
 import { getCookie } from 'cookies-next'
 import { GetServerSideProps } from 'next'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface Props {
   name: string
@@ -23,9 +27,27 @@ const meta: NavbarProps = {
   icon: navInfo.icon,
 }
 
-export const BukuBesarPage = ({ name, accountId, entries }: Props) => {
+export const BukuBesarPage = ({ name, accountId }: Props) => {
   const { accounts } = useAccount()
   const account = accounts.find(x => x.id === accountId)!
+
+  const [entries, setEntries] = useState<JournalEntry[]>([])
+  const { userProfile } = useAuth()
+  const { organizationView } = useOrganization()
+
+  useEffect(() => {
+    const fetchAccounts = async () => {
+      if (userProfile?.role === ROLE.LEMBAGA) {
+        const { data: { data } } = await getJournalEntriesByAccount(accountId, userProfile.organization.id)
+        setEntries(data)
+      } else {
+        const { data: { data } } = await getJournalEntriesByAccount(accountId, organizationView?.id)
+        setEntries(data)
+      }
+    }
+    fetchAccounts()
+  }, [])
+
 
   return (
     <Layout navbarProps={{ ...meta, pageName: name }}>
@@ -34,15 +56,11 @@ export const BukuBesarPage = ({ name, accountId, entries }: Props) => {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ params, req, res }) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const accountParam = params?.name
   const { name, accountId } = extractAccountInfo(accountParam)
 
-  const token = getCookie('token', { req, res }) as string
-  if (token) axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-  const { data: { data } } = await getJournalEntriesByAccount(accountId)
-
-  return { props: { name, accountId, entries: data } }
+  return { props: { name, accountId } }
 }
 
 export default BukuBesarPage
